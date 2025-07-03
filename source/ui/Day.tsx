31ff1f59d3101c37b7da5d08a3bb564a549fe12a
Filippo Dembech <filippo.dembech@gmail.com>
2025-07-03 14:59:12 +0200

    add: new tab keymap

--- source/ui/Day.tsx
+++ source/ui/Day.tsx
@@ -3,14 +3,13 @@ import {DayType} from '../types.js';
 import React, {useState} from 'react';
 import BigText from 'ink-big-text';
 import {Tab, Tabs} from 'ink-tab';
-import { Focus } from '../Focus.js';
-import { Task, TaskList } from 'ink-task-list';
-
+import {Focus} from '../Focus.js';
+import {Task, TaskList} from 'ink-task-list';
 
 export default function Day({day, ...props}: {day: DayType} & BoxProps) {
 	const [activeTabName, setActiveTabName] = useState<string>();
-	
-	const {isFocused} = useFocus({ id: Focus.dayTabs })
+
+	const {isFocused} = useFocus({id: Focus.dayTabs});
 
 	const categoryNames = [
 		...new Set(day.thoughts.map(thought => thought.category?.name)),
@@ -21,15 +20,20 @@ export default function Day({day, ...props}: {day: DayType} & BoxProps) {
 	}
 
 	return (
-		<Box borderStyle="round" alignItems={categoryNames.length === 0 ? "center" : "stretch"} gap={8} {...props}>
+		<Box
+			borderStyle="round"
+			alignItems={categoryNames.length === 0 ? 'center' : 'stretch'}
+			gap={8}
+			{...props}
+		>
 			<BigText text={day.date} font="tiny" />
 			<Box flexDirection="column" gap={1}>
 				{categoryNames.length ? (
 					<Tabs
 						keyMap={{
 							useNumbers: true,
-							previous: ['j'],
-							next: ['k']
+							previous: ['h', 'j'],
+							next: ['k', 'l'],
 						}}
 						onChange={handleTabChange}
 						colors={{activeTab: {color: 'blue', backgroundColor: 'black'}}}
@@ -41,14 +45,18 @@ export default function Day({day, ...props}: {day: DayType} & BoxProps) {
 							</Tab>
 						))}
 					</Tabs>
-				) : <Text italic color="gray">No thoughts yet</Text>}
+				) : (
+					<Text italic color="gray">
+						No thoughts yet
+					</Text>
+				)}
 				<Box flexDirection="column">
 					{day.thoughts
 						.filter(thought => thought.category?.name === activeTabName)
 						.map((thought, i) => (
-								<TaskList key={`thought-${i}`}>
-									<Task label={thought.content} state="pending"/>
-								</TaskList>
+							<TaskList key={`thought-${i}`}>
+								<Task label={thought.content} state="pending" />
+							</TaskList>
 						))}
 				</Box>
 			</Box>
