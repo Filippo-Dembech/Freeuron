aaa2721fcff8abdc26e9b8a20a4896f3346db80e
Filippo Dembech <filippo.dembech@gmail.com>
2025-07-04 18:21:11 +0200

    style: gradient to heading

--- source/ui/Heading.tsx
+++ source/ui/Heading.tsx
@@ -1,14 +1,17 @@
 import {Box} from 'ink';
 import React from 'react';
 import Title from '../components/Title.js';
-import { useDay } from '../context/DayContext.js';
+import {useDay} from '../context/DayContext.js';
+import Gradient from 'ink-gradient';
 
 export default function Heading() {
-	
-	const { today } = useDay();
+	const {today} = useDay();
+
 	return (
 		<Box alignItems="flex-end">
-			<Title color="blue">Freeuron</Title>
+			<Gradient name="mind">
+				<Title color="blue">Freeuron</Title>
+			</Gradient>
 			<Title> - </Title>
 			<Title>{today.date}</Title>
 		</Box>
