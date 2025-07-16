c3393a63508a8a784f6927245870c6d5291b126c
Filippo Dembech <filippo.dembech@gmail.com>
2025-07-16 10:21:47 +0200

    fix: UI glich in vertical category tabs

--- source/pages/BrowsePage.tsx
+++ source/pages/BrowsePage.tsx
@@ -14,50 +14,48 @@ export default function BrowsePage() {
 	const {isFocused} = useFocus({id: Focus.filterTabs});
 
 	return (
-		<Box>
-			<Box gap={4}>
-				<Box padding={2}>
-					<Tabs
-						colors={{
-							activeTab: {color: 'blue', backgroundColor: 'black'},
-						}}
-						isFocused={isFocused}
-						flexDirection="column"
-						keyMap={{
-							useNumbers: true,
-							previous: ['k', 'l'],
-							next: ['h', 'j'],
-						}}
-						onChange={tabName =>
-							setCategory(categories.find(c => c.name === tabName))
-						}
-					>
-						{categories.map((category, i) => (
-							<Tab
-								key={`${category.name}-${category.placeholder}-${i}`}
-								name={category.name}
-							>
-								{category.name}
-							</Tab>
+		<Box gap={7}>
+			<Box padding={2}>
+				<Tabs
+					colors={{
+						activeTab: {color: 'blue', backgroundColor: 'black'},
+					}}
+					isFocused={isFocused}
+					flexDirection="column"
+					keyMap={{
+						useNumbers: true,
+						previous: ['k', 'l'],
+						next: ['h', 'j'],
+					}}
+					onChange={tabName =>
+						setCategory(categories.find(c => c.name === tabName))
+					}
+				>
+					{categories.map((category, i) => (
+						<Tab
+							key={`${category.name}-${category.placeholder}-${i}`}
+							name={category.name}
+						>
+							{category.name}
+						</Tab>
+					))}
+				</Tabs>
+			</Box>
+			<Box borderStyle="single" flexGrow={1}>
+				{category ? (
+					<Box flexDirection="column" paddingLeft={3} paddingBottom={2}>
+						<BigText text={category.name} font="tiny" />
+						{getAll(category).map(thought => (
+							<Task
+								key={thought.id}
+								label={thought.content}
+								state={thought.checked ? 'success' : 'pending'}
+							/>
 						))}
-					</Tabs>
-				</Box>
-				<Box  borderStyle="single" width="100%">
-					{category ? (
-						<Box flexDirection="column" paddingLeft={3} paddingBottom={2}>
-							<BigText text={category.name} font='tiny'/>
-							{getAll(category).map(thought => (
-								<Task
-									key={thought.id}
-									label={thought.content}
-									state={thought.checked ? 'success' : 'pending'}
-								/>
-							))}
-						</Box>
-					) : (
-						<Text>No thought</Text>
-					)}
-				</Box>
+					</Box>
+				) : (
+					<Text>No thought</Text>
+				)}
 			</Box>
 		</Box>
 	);
